9edbc58f0c1c71c536e3d027f96e3b8f66397286
César González <chesarglez429@gmail.com>
2021-07-02 18:53:39 -0500

    :lipstick: Style the page like a paper

--- src/Main.elm
+++ src/Main.elm
@@ -90,14 +90,53 @@ view { theme } =
             [ minWidth (vw 100)
             , minHeight (vh 100)
             , myMainBackground theme
+            , displayFlex
+            , justifyContent center
             ]
         ]
-        [ span
-            [ onClick ChangeTheme
-            , css [ cursor pointer ]
+        [ div
+            [ css
+                [ maxWidth (rem 38.25)
+                , width (pct 100)
+                , height (rem 49.5)
+                , borderRadius (px 10)
+                , margin2 (rem 2) (px 0)
+                , padding (rem 1)
+                , myPaperBackground theme
+                , paperShadow theme
+                ]
             ]
-            [ switchThemeIcon 16 theme ]
+            [ span
+                [ onClick ChangeTheme
+                , css [ cursor pointer ]
+                ]
+                [ switchThemeIcon 16 theme ]
+            ]
         ]
+
+
+smoothGrayShadow : Style
+smoothGrayShadow =
+    property "box-shadow"
+        (String.concat
+            [ "0 2.8px 2.2px rgba(0, 0, 0, 0.034),"
+            , "0 6.7px 5.3px rgba(0, 0, 0, 0.048),"
+            , "0 12.5px 10px rgba(0, 0, 0, 0.06),"
+            , "0 22.3px 17.9px rgba(0, 0, 0, 0.072),"
+            , "0 41.8px 33.4px rgba(0, 0, 0, 0.086),"
+            , "0 100px 80px rgba(0, 0, 0, 0.12)"
+            ]
+        )
+
+
+paperShadow : Theme -> Style
+paperShadow theme =
+    case theme of
+        Dark ->
+            batch []
+
+        Light ->
+            smoothGrayShadow
 
 
 switchThemeIcon : Int -> Theme -> Svg.Styled.Svg msg
@@ -114,7 +153,17 @@ myMainBackground : Theme -> Style
 myMainBackground theme =
     case theme of
         Dark ->
-            backgroundColor (hex "141414")
+            backgroundColor (hex "121212")
 
         Light ->
-            backgroundColor (hex "ededed")
+            backgroundColor (hex "EEF2F7")
+
+
+myPaperBackground : Theme -> Style
+myPaperBackground theme =
+    case theme of
+        Dark ->
+            backgroundColor (hex "2C2C2C")
+
+        Light ->
+            backgroundColor (hex "FFFFFF")
