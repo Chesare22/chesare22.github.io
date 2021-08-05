3844aa5b4ddca6446d50525ad417a9793b1c8e8f
César González <chesarglez429@gmail.com>
2021-08-05 02:24:33 -0500

    :sparkles: Add an event listener to hide bar on scroll

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import './main.css';
-import { Elm } from './Main.elm';
-import * as serviceWorker from './serviceWorker';
+import "./main.css";
+import { Elm } from "./Main.elm";
+import * as serviceWorker from "./serviceWorker";
 
 Elm.Main.init({
-  node: document.getElementById('root')
+  node: document.getElementById("root"),
+});
+
+window.addEventListener("load", () => {
+  const optionsBar = document.getElementById("options-bar");
+
+  const getCurrentScroll = () =>
+    window.scrollY || document.documentElement?.scrollTop;
+
+  let previousScroll = getCurrentScroll();
+  let wasScrolling = false;
+
+  window.addEventListener("scroll", () => {
+    const currentScroll = getCurrentScroll();
+
+    if (currentScroll > previousScroll && !wasScrolling) {
+      optionsBar.classList.add("hidden");
+      wasScrolling = true;
+    } else if (currentScroll < previousScroll && wasScrolling) {
+      optionsBar.classList.remove("hidden");
+      wasScrolling = false;
+    }
+
+    previousScroll = currentScroll;
+  });
 });
 
 // If you want your app to work offline and load faster, you can change
