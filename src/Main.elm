846b9fc008823e1f82e497d7d340ca923c2be09c
César González <chesarglez429@gmail.com>
2021-08-07 19:08:07 -0500

    :sparkles: Remove extra whitespaces of multiline string

--- src/Main.elm
+++ src/Main.elm
@@ -12,6 +12,7 @@ import Html.Styled.Events exposing (..)
 import Material.Icons as Filled
 import Material.Icons.Types exposing (Coloring(..))
 import Phone
+import Regex
 import Svg.Styled
 
 
@@ -431,8 +432,8 @@ displayGrid =
 
 smoothGrayShadow : Style
 smoothGrayShadow =
-    property "box-shadow"
-        """
+    property "box-shadow" <|
+        removeExtraWhitespaces """
             0 2.8px 2.2px rgba(0, 0, 0, 0.034),
             0 6.7px 5.3px rgba(0, 0, 0, 0.048),
             0 12.5px 10px rgba(0, 0, 0, 0.06),
@@ -440,6 +441,16 @@ smoothGrayShadow =
             0 41.8px 33.4px rgba(0, 0, 0, 0.086),
             0 100px 80px rgba(0, 0, 0, 0.12)
         """
+
+
+removeExtraWhitespaces : String -> String
+removeExtraWhitespaces =
+    Regex.replace
+        (Maybe.withDefault Regex.never <|
+            Regex.fromString "\\s\\s+"
+        )
+        (always " ")
+        >> String.trim
 
 
 
