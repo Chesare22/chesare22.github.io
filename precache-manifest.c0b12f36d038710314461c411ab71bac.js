2779f0fc81478287616857089f598b6f5b8aef57
César González <chesarglez429@gmail.com>
2021-08-28 12:40:04 -0500

    Updates

---
+++ precache-manifest.c0b12f36d038710314461c411ab71bac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/curriculum/favicon.ico"
+  },
+  {
+    "revision": "bd05bb8652f2a03ac2a446739f6cc81d",
+    "url": "/curriculum/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/curriculum/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/curriculum/manifest.json"
+  },
+  {
+    "revision": "bcfa8a415017fa4c8ed6",
+    "url": "/curriculum/static/css/main.2dbdc024.chunk.css"
+  },
+  {
+    "revision": "bcfa8a415017fa4c8ed6",
+    "url": "/curriculum/static/js/main.bcfa8a41.chunk.js"
+  },
+  {
+    "revision": "d4d64f1720b64d625552",
+    "url": "/curriculum/static/js/runtime~main.d4d64f17.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/curriculum/static/js/vendors~main.0b49ec49.chunk.js"
+  },
+  {
+    "revision": "a059222ec873c4df05783551da524302",
+    "url": "/curriculum/static/media/profile.976e6108.jpg"
+  }
+]);
