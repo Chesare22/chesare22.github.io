b85732c796ea40403135f4eaea177546012b5c19
César González <chesarglez429@gmail.com>
2024-01-30 21:48:14 -0600

    :construction: Use lorem ipsum to fill job descriptions

--- src/Constants.elm
+++ src/Constants.elm
@@ -134,7 +134,7 @@ jobs =
         (Language.translated "Desarrollador Web Frontend" "Frontend Web Developer")
         (Language.translated
             "SoldAI es una empresa yucateca dedicada a la inteligencia artificial. Ayudé en pruebas, desarrollo, mantenimiento y documentación de varios proyectos."
-            "SoldAI is a yucatecan company dedicated to the artificial intelligence. I helped in testing, developing, maintenance and documentation of various projects."
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         )
     , Experience (always "Sumerian")
         (SimpleDate Time.Jun 2020)
@@ -142,7 +142,7 @@ jobs =
         (Language.translated "Ingeniero de Software" "Software Engineer")
         (Language.translated
             "Sumerian hace aplicaciones web. En cada proyecto experimentábamos con nuevas herramientas y metodologías."
-            "Sumerian makes custom web applications. On every project we experimented with new tools and methodologies."
+            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
         )
     , Experience (always "Coatí Labs")
         (SimpleDate Time.Jan 2021)
@@ -150,7 +150,7 @@ jobs =
         (Language.translated "Desarrollador Web Frontend" "Frontend Web Developer")
         (Language.translated
             "Empresa de programación web donde me familiaricé con algunos eventos de scrum. Mi labor principal era el desarrollo de páginas web con React y TypeScript."
-            "A web-programming company where I got familiarized with some scrum events. My primary labour was to develop web pages with React and TypeScript."
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus."
         )
     , Experience (always "MID Software")
         (SimpleDate Time.Mar 2022)
